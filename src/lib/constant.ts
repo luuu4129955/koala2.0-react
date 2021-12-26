072d6998ade7afe0058d2418baf6251335bda514
crystalpeng <crystalpeng1998@gmail.com>
2021-12-26 22:02:39 +0800

    声明了常量存放默认标签组

---
+++ src/lib/constant.ts
@@ -0,0 +1,22 @@
+// @ts-ignore
+const defaultTags: Tag[] = [
+  {id: 'foods', name: '餐饮'},
+  {id: 'athletics', name: '运动'},
+  {id: 'clothes', name: '服饰'},
+  {id: 'digital', name: '数码'},
+  {id: 'donate', name: '捐赠'},
+  {id: 'education', name: '教育'},
+  {id: 'entertainment', name: '娱乐'},
+  {id: 'games', name: '游戏'},
+  {id: 'home', name: '家居'},
+  {id: 'makeup', name: '美妆'},
+  {id: 'medical', name: '医疗'},
+  {id: 'objects', name: '日用品'},
+  {id: 'pets', name: '宠物'},
+  {id: 'shopping', name: '购物'},
+  {id: 'social', name: '社交'},
+  {id: 'traffic', name: '交通'},
+  {id: 'others', name: '其他'},
+];
+
+export default defaultTags

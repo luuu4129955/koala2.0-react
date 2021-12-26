0d44fff7725c9db9a7fd36e781b558c65d7c8111
crystalpeng <crystalpeng1998@gmail.com>
2021-12-26 22:04:22 +0800

    Tags组件 遍历数组生成默认标签

--- src/components/Money/Tags.tsx
+++ src/components/Money/Tags.tsx
@@ -1,54 +1,67 @@
-import Icon from "components/Icon";
-import styled from "styled-components";
+import Icon from 'components/Icon';
+import {useState} from 'react';
+import styled from 'styled-components';
+import "lib/constant"
+import defaultTags from 'lib/constant';
 
-const Tags=()=>{
-  return(
-    <TagsSection>
-        <ul>
-          <li><Icon name="clothes"/><span>衣</span></li>
-          <li><Icon name="foods"/><span>食</span></li>
-        </ul>
-    </TagsSection>
-  )
+type Tag = {
+  id: string,
+  name: string
 }
-export default Tags
+const Tags: React.FC = () => {
+  const [tags, setTags] = useState<Tag[]>(defaultTags);
+  const onAddTag = () => {
+    const newTag=window.prompt('要添加的标签名为：')
+  }
+  return (
+    <TagsWraper>
+      <ul>
+        {/*这里icon占位了但是显示不出来*/}
+        {tags.map(tag =>
+          <li key={tag.id}>
+            <Icon name={tag.id}/>
+            <span>{tag.name}</span>
+          </li>)}
+        <li onClick={onAddTag}><Icon name="add"/><span>自定义</span></li>
+      </ul>
+    </TagsWraper>
+  );
+};
+export default Tags;
 
-const TagsSection = styled.section`
-    flex-grow: 1;
-    overflow: auto;
-    background-color: #fff;
-    padding: 4px;
-    li {
-      display: inline-flex;
-      flex-direction: column;
-      width: 56px;
-      margin: 1px;
+const TagsWraper = styled.section`
+  flex-grow: 1;overflow: auto;background-color: #fff;padding: 4px;
+
+  li {
+    display: inline-flex;flex-direction: column;
+    width: 56px;margin: 1px;
+
+    .icon {
+      padding: 8px;margin: 0 auto;fill: #888;
+    }
+
+    span {
+      color: #888;
+      height: 24px;line-height: 24px;
+      font-size: 12px;text-align: center;
+    }
+
+    &.selected {
+      border-radius: 50%;background-color: #eeffed;
+
+      &:active {
+        transform: translateY(4px);
+        box-shadow: 0 2px #a2dd9e;
+      }
+
       .icon {
-        padding: 8px;
-        margin: 0 auto;
-        fill: #888;
+        fill: #a2dd9e;
       }
+
       span {
-        color: #888;
-        height: 24px;
-        line-height: 24px;
-        font-size: 12px;
-        text-align: center;
-      }
-      &.selected {
-        border-radius: 50%;
-        background-color: #eeffed;
-        &:active {
-          transform: translateY(4px);
-          box-shadow: 0 2px #a2dd9e;
-        }
-        .icon {
-          fill: #a2dd9e;
-        }
-        span {
-          color: #a2dd9e;
-        }
+        color: #a2dd9e;
       }
     }
+  }
 `;
 

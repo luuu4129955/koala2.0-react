0dc4e4e1f6d9f1db5582eb366eeec7aa8ba2c63f
crystalpeng <crystalpeng1998@gmail.com>
2021-12-26 23:01:44 +0800

    优化Board组件样式

--- src/components/Money/Board.tsx
+++ src/components/Money/Board.tsx
@@ -52,33 +52,27 @@ const BoardSection = styled.section`
       width: 80%;
       grid-template-columns: repeat(3, 33.3333%);
       grid-template-rows: repeat(5, 25%);
-      $bg:red
       .zero{
         grid-column-start: 2;
         grid-column-end: 4;
+        background-color: #b8b8b8;
       }
       button {
         padding: 16px 32px;
         &:nth-child(1) {
-          background: $bg;
+          background: #f5f5f5;
         }
-        &:nth-child(2), &:nth-child(5) {
-          background: darken($bg, 4%);
+        &:nth-child(2), &:nth-child(4) {
+          background: #ebebeb;
         }
-        &:nth-child(3), &:nth-child(6), &:nth-child(9) {
-          background: darken($bg, 4%*2);
+        &:nth-child(3), &:nth-child(5), &:nth-child(7) {
+          background: #e1e1e1;
         }
-        &:nth-child(4), &:nth-child(7), &:nth-child(10), &:nth-child(13) {
-          background: darken($bg, 4%*3);
+        &:nth-child(6), &:nth-child(8), &:nth-child(10){
+          background: #d6d6d6;
         }
-        &:nth-child(8), &:nth-child(11), &:nth-child(14) {
-          background: darken($bg, 4%*4);
-        }
-        &:nth-child(12), &:nth-child(15) {
-          background: darken($bg, 4%*5);
-        }
-        &:nth-child(16) {
-          background: darken($bg, 4%*6);
+        &:nth-child(9) {
+          background: #c2c2c2;
         }
       }
     }

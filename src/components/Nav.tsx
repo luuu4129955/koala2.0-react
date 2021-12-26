35537f40f2600c23353d6ac49f2096b8e5553b0d
crystalpeng <crystalpeng1998@gmail.com>
2021-12-26 14:20:00 +0800

    修改Nav组件样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ import Icon from './Icon';
 const NavWrapper = styled.div`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-
+  background-color: #fff;
   > ul {
     display: flex;
     color: #888888;

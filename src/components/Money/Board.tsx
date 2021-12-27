66cff92d101e1d808ab334291915fd4f4e37fe2f
crystalpeng <crystalpeng1998@gmail.com>
2021-12-27 23:36:13 +0800

    遍历生成数字面板

--- src/components/Money/Board.tsx
+++ src/components/Money/Board.tsx
@@ -1,14 +1,15 @@
 import {useState} from 'react';
-import {BoardSection} from './Board/BoardSection'
-import {calculateOutput} from './Board/calculateOutput'
+import {BoardSection} from './Board/BoardSection';
+import {calculateOutput} from './Board/calculateOutput';
 
 const Board: React.FC = () => {
+  const numbesList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
   const [output, setOutput] = useState('0');
   const onClickNumber = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) return;
-    else if(output.length>=16)return;
-    setOutput(calculateOutput(text,output))
+    else if (output.length >= 16) return;
+    setOutput(calculateOutput(text, output));
   };
   const backspace = () => {
     if (output.length === 1) {
@@ -16,26 +17,17 @@ const Board: React.FC = () => {
     } else {
       setOutput(output.slice(0, -1));
     }
-  }
+  };
   const clear = () => {
-    setOutput('0')
-  }
+    setOutput('0');
+  };
   return (
     <>
       <BoardSection>
         <div className="board-output">{output}</div>
         <div className="board-input">
           <div className="numbers" onClick={onClickNumber}>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>.</button>
+            {numbesList.map(n => <button key={n}>{n}</button>)}
             <button className="zero">0</button>
           </div>
           <div className="result">

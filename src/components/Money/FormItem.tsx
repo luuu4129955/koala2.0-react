f5e7a8293de9f9430f850b7a8a8df3dbd76f5a6a
crystalpeng <crystalpeng1998@gmail.com>
2021-12-27 15:30:37 +0800

    完成表单组件

--- src/components/Money/FormItem.tsx
+++ src/components/Money/FormItem.tsx
@@ -1,20 +1,31 @@
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
 
-const FormItem = () => {
+const FormItem: React.FC = () => {
+  const [note,setNote]=useState('')
+  const refInput=useRef<HTMLInputElement>(null)
+  const onBlur = () => {
+    if (refInput.current!==null){
+      setNote(refInput.current.value)
+    }
+  };
+
   return (
     <FormItemSection>
-      备注：<input/>
+      备注：<input type="text" placeholder="在这里添加备注~"
+                ref={refInput} onBlur={onBlur}/>
     </FormItemSection>
   );
 };
 export default FormItem;
 
 const FormItemSection = styled.section`
-    padding: 10px 10px;
-    display: flex;
-    align-items: center;
-    input {
-      font-size: inherit;
-      padding-left: 16px;
-    }
+  padding: 10px 10px;
+  display: flex;
+  align-items: center;
+
+  input {
+    font-size: inherit;
+    padding-left: 16px;
+  }
 `;

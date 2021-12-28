2ab3a64d58f360ae985781ef23c990118c96feb4
crystalpeng <crystalpeng1998@gmail.com>
2021-12-28 22:22:02 +0800

    重构 集合Money组件数据

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,27 +2,26 @@ import Board from 'components/Money/Board';
 import FormItem from 'components/Money/FormItem';
 import Tabs from 'components/Money/Tabs';
 import Tags from 'components/Money/Tags';
-import { useState } from 'react';
+import {useState} from 'react';
 
-type Category='-'|'+'
+type Category = '-' | '+'
 
 function Money() {
-  const [record,setRecord]=useState({
-    tag:{id:"foods",name:"餐饮"} ,
-    note:'',
-    category:'-' as Category,
-    amount:'0'
-  })
+  const [record, setRecord] = useState({
+    tag: {id: 'foods', name: '餐饮'},
+    note: '',
+    category: '-' as Category,
+    amount: '0'
+  });
+  const onChange = (obj: Partial<typeof record>) => {
+    setRecord({...record, ...obj});
+  };
   return (
     <>
-      {record.tag.name}
-      {record.note}
-      {record.category}
-      {record.amount}
-      <Tags value={record.tag} onChange={(tag)=>setRecord({...record,tag})}></Tags>
-      <FormItem value={record.note} onChange={(note)=>setRecord({...record,note})}></FormItem>
-      <Tabs value={record.category} onChange={(category)=>setRecord({...record,category})}></Tabs>
-      <Board value={record.amount} onChange={(amount)=>setRecord({...record,amount})}></Board>
+      <Tags value={record.tag} onChange={tag => onChange({tag})}></Tags>
+      <FormItem value={record.note} onChange={note => onChange({note})}></FormItem>
+      <Tabs value={record.category} onChange={category => onChange({category})}></Tabs>
+      <Board value={record.amount} onChange={amount => onChange({amount})}></Board>
     </>
   );
 }

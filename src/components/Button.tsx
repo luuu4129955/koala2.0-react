cf1a4e6f44daf67702a9271375f4d51615ec09da
crystalpeng <crystalpeng1998@gmail.com>
2021-12-29 21:14:01 +0800

    修改Button组件

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,20 @@
 import styled from 'styled-components';
-type Props={
-  name:string
+
+type Props = {
+  name: string
 }
-const Button= (props:Props) => {
+const Button = (props: Props) => {
   return (
-    <Btn>
-      <div className="btn">{props.name}</div>
-    </Btn>
+    <Btn>{props.name}</Btn>
   );
 };
 
-const Btn=styled.div`
-  .btn {
-    text-align: center;
-    margin: 64px 18px 64px 18px;
-    padding: 9px 18px;
-    border-radius: 8px;
-    color: #fff;
-    letter-spacing: 6px;
-  }
-`
-export {Button}
+const Btn = styled.div`
+  text-align: center;
+  margin: 64px 18px 64px 18px;
+  padding: 9px 18px;
+  border-radius: 8px;
+  color: #fff;
+  letter-spacing: 6px;
+`;
+export {Button};
